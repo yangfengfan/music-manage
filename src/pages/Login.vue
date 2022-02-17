6687c7573ac9a7438e6ac0d1873658434abf819f
yangfengfan <524046470@qq.com>
2022-02-17 16:50:40 +0800

    Initial commit

---
+++ src/pages/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="login-wrap">
+    <div class="ms-title">music 后台管理登录</div>
+    <div class="ms-login">
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
+        <el-form-item prop="userName">
+          <el-input v-model="ruleForm.username"  placeholder="用户名"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input type="password" v-model="ruleForm.password"  placeholder="用户名"></el-input>
+        </el-form-item>
+        <div class="login-btn">
+          <el-button type="primary" @click="submitForm">登录</el-button>
+        </div>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.login-wrap {
+  position: relative;
+  background: url("../assets/img/background.jpg");
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+}
+.ms-title {
+  position: absolute;
+  top: 50%;
+  width: 100%;
+  margin-top: -230px;
+  text-align: center;
+  font-size: 30px;
+  font-weight: 600;
+  color: #fff;
+}
+.ms-login{
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  width: 300px;
+  height: 160px;
+  margin-left: -190px;
+  margin-top: -150px;
+  padding: 40px;
+  border-radius: 5px;
+  background: #fff;
+}
+.login-btn{
+
+}
+</style>
